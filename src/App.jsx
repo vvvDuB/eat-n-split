7c3a7fd8869b3cf9c38679b41e321a4d959fcea2
atef <atef.dev@icloud.com>
2024-06-02 14:13:01 +0200

    App(working): add users and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-function App() {
+const Friends = [
+  {
+    id: 0,
+    img: "https://i.pravatar.cc/",
+    name: "Mario",
+    balance: 150,
+  },
+  {
+    id: 1,
+    img: "https://i.pravatar.cc/",
+    name: "Mario",
+    balance: 150,
+  },
+  {
+    id: 2,
+    img: "https://i.pravatar.cc/",
+    name: "Mario",
+    balance: 150,
+  },
+];
+
+const App = () => {
   return <h1>ciao</h1>;
-}
+};
+
+const List = () => {
+  return;
+};
+
+const User = () => {
+  return;
+};
 
 export default App;
